528896ee5ac4f2c243a9c550b397c79f4445dbf8
slady <csl.bunny.2207@gmail.com>
2023-04-10 22:50:57 +0200

    buttons

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+const input = document.querySelector(".input");
+
+const btnAddTask = document.querySelector(".btn__add-task");
+
+const tasks = document.querySelector(".tasks-container");
+const task = document.querySelector(".task");
+const taskName = document.querySelector(".task-name");
+const btnEditTask = document.querySelector(".btn__edit-task");
+const btnDoneTask = document.querySelector(".btn__finish-task");
+const btnDeleteTask = document.querySelector(".btn__delete-task");
+
+btnAddTask.addEventListener("click", function () {
+  const text = input.value;
+  console.log(text);
+  taskName.innerHTML = text;
+  input.value = "";
+});
+
+btnEditTask.addEventListener("click", function () {
+  // input.value = ;
+  taskName.innerHTML = "";
+});
