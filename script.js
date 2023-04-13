60f10285cfcd6266f4029995b23ddc844471e466
slady <csl.bunny.2207@gmail.com>
2023-04-13 22:35:27 +0200

    edit btn

--- script.js
+++ script.js
@@ -4,38 +4,43 @@ const input = document.querySelector(".input");
 const btnAddTask = document.querySelector(".btn__add-task");
 
 const tasks = document.querySelector(".tasks-container");
-const task = document.querySelector(".task");
-const taskName = document.querySelector(".task-name");
-const btnEditTask = document.querySelector(".btn__edit-task");
-const btnDoneTask = document.querySelector(".btn__finish-task");
-const btnDeleteTask = document.querySelector(".btn__delete-task");
-
-const html = `<div class="task">
-<p class="task-name"></p>
-<button class="btn__edit-task">edit</button>
-<button class="btn__finish-task">done</button>
-<button class="btn__delete-task">delete</button>
-</div>`;
+// const task = document.querySelector(".task");
 
 btnAddTask.addEventListener("click", function (e) {
   e.preventDefault();
 
   const text = input.value;
-  console.log(text);
-  taskName.innerHTML = text;
-  input.value = "";
-  tasks.insertAdjacentHTML("afterbegin", html);
+  if (text !== "") {
+    const newTask = document.createElement("div");
+    newTask.className = "task";
+    newTask.innerHTML = `<p class="task-name">${text}</p>
+    <button class="btn__edit-task">edit</button>
+    <button class="btn__finish-task">done</button>
+    <button class="btn__delete-task">delete</button>`;
+    tasks.appendChild(newTask);
+    input.value = "";
+  }
 });
 
-btnEditTask.addEventListener("click", function () {
-  input.value = taskName.innerHTML;
-  taskName.innerHTML = "";
-});
+tasks.addEventListener("click", function (e) {
+  const task = e.target.closest(".task");
 
-btnDoneTask.addEventListener("click", function () {
-  taskName.classList.toggle("task-done");
-});
+  const taskName = document.querySelector(".task-name");
+  const btnEditTask = document.querySelector(".btn__edit-task");
+  const btnDoneTask = document.querySelector(".btn__finish-task");
+  const btnDeleteTask = document.querySelector(".btn__delete-task");
 
-btnDeleteTask.addEventListener("click", function () {
-  task.remove();
+  if (e.target === btnEditTask) {
+    if (taskName.textContent !== "") {
+      input.value = taskName.textContent;
+      taskName.textContent = "";
+    } else {
+      taskName.textContent = input.value;
+      input.value = "";
+    }
+  } else if (e.target === btnDoneTask) {
+    taskName.classList.toggle("task-done");
+  } else if (e.target === btnDeleteTask) {
+    tasks.removeChild(task);
+  }
 });
