e1a5b4f28478091d829bea211343f34743955322
slady <csl.bunny.2207@gmail.com>
2023-04-16 22:39:09 +0200

    style

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
 "use strict";
+
+///// Elements
 const input = document.querySelector(".input");
-
-const btnAddTask = document.querySelector(".btn__add-task");
-
+const btnAddTask = document.querySelector(".btn--add-task");
 const tasks = document.querySelector(".tasks-container");
 
-// const addEventListeners = function () {
-//   const editButtons = document.querySelectorAll(".btn__edit-task");
-//   const doneButtons = document.querySelectorAll(".btn__finish-task");
-//   const deleteButtons = document.querySelectorAll(".btn__delete-task");
+//// Handlers
+const handleDoneButtonClick = function (e) {
+  const taskElement = e.target.closest(".task");
+  const taskName = taskElement.querySelector(".task-name");
+  taskName.classList.toggle("task-done");
+};
 
-//   editButtons.forEach(function (btn) {
-//     btn.addEventListener("click", function (e) {
-//       const taskElement = e.target.closest(".task");
-//       const taskName = taskElement.querySelector(".task-name");
+const handleDeleteButtonClick = function (e) {
+  const taskElement = e.target.closest(".task");
+  taskElement.remove();
+};
 
-//       if (taskName.textContent !== "") {
-//         input.value = taskName.textContent;
-//         taskName.textContent = "";
-//       } else {
-//         taskName.textContent = input.value;
-//         input.value = "";
-//       }
-//     });
-//   });
+const handleTaskNameKeydown = function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    e.target.blur();
+  }
+};
 
-//   doneButtons.forEach(function (btn) {
-//     btn.addEventListener("click", function (e) {
-//       const taskElement = e.target.closest(".task");
-//       const taskName = taskElement.querySelector(".task-name");
-//       taskName.classList.toggle("task-done");
-//     });
-//   });
+///// Events
+const addEventListeners = function () {
+  const doneButtons = document.querySelectorAll(".btn--finish-task");
+  const deleteButtons = document.querySelectorAll(".btn--delete-task");
+  const taskName = document.querySelectorAll(".task-name");
 
-//   deleteButtons.forEach(function (btn) {
-//     btn.addEventListener("click", function (e) {
-//       const taskElement = e.target.closest(".task");
-//       taskElement.remove();
-//     });
-//   });
-// };
+  doneButtons.forEach(function (btn) {
+    btn.removeEventListener("click", handleDoneButtonClick);
+    btn.addEventListener("click", handleDoneButtonClick);
+  });
 
-// addEventListeners();
+  deleteButtons.forEach(function (btn) {
+    btn.removeEventListener("click", handleDeleteButtonClick);
+    btn.addEventListener("click", handleDeleteButtonClick);
+  });
+
+  taskName.forEach(function (name) {
+    name.removeEventListener("keydown", handleTaskNameKeydown);
+    name.addEventListener("keydown", handleTaskNameKeydown);
+  });
+};
 
 btnAddTask.addEventListener("click", function (e) {
   e.preventDefault();
@@ -51,43 +54,10 @@ btnAddTask.addEventListener("click", function (e) {
     const newTask = document.createElement("div");
     newTask.className = "task";
     newTask.innerHTML = `<p class="task-name" contenteditable="true">${text}</p>
-    <button class="btn__edit-task">edit</button>
-    <button class="btn__finish-task">done</button>
-    <button class="btn__delete-task">delete</button>`;
+    <button class="btn--finish-task">done</button>
+    <button class="btn--delete-task">delete</button>`;
     tasks.appendChild(newTask);
     input.value = "";
     addEventListeners();
   }
-});
-
-tasks.addEventListener("click", function (e) {
-  const task = e.target.closest(".task");
-
-  // const taskName = e.target.closest(".task-name");
-  // const btnEditTask = e.target.closest(".btn__edit-task");
-  // const btnDoneTask = e.target.closest(".btn__finish-task");
-  // const btnDeleteTask = e.target.closest(".btn__delete-task");
-
-  const taskName = document.querySelector(".task-name");
-  const btnEditTask = document.querySelector(".btn__edit-task");
-  const btnDoneTask = document.querySelector(".btn__finish-task");
-  const btnDeleteTask = document.querySelector(".btn__delete-task");
-
-  if (e.target === btnEditTask) {
-    if (taskName.textContent !== "") {
-      input.value = taskName.textContent;
-      taskName.textContent = "";
-    } else {
-      taskName.textContent = input.value;
-      input.value = "";
-    }
-  } else if (e.target === btnDoneTask) {
-    taskName.classList.toggle("task-done");
-  } else if (e.target === btnDeleteTask) {
-    tasks.removeChild(task);
-  }
-
-  taskName.addEventListener("keydown", function (e) {
-    e.preventDefault();
-  });
 });
